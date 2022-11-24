3b2931aee62f64f332e0cb555be9085c7cfdd672
monje7 <juancarlosmonjeforero@gmail.com>
2022-11-24 08:20:25 -0500

    confirmacion de modificaciones

--- js/M-QUERY.js
+++ js/M-QUERY.js
@@ -1,7 +1,10 @@
+
+
 let barMenu = document.getElementById("barMenu");
 let main_Menu = document.getElementById("mainMenu");
 
 barMenu.addEventListener("click", function() {
+    
     if (main_Menu.classList.contains("main-menu")) {
         main_Menu.classList.add("menu-bloque-visible");
         main_Menu.classList.remove("menu-bloque-oculto");
